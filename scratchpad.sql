2c7d7c178ada3db1088b99bbf17a723faab6d916
TJ Stalcup <stalcuptj@america.gov>
2020-10-17 18:20:05 -0400

    Adds in Migrations

---
+++ scratchpad.sql
@@ -0,0 +1,3 @@
+--INSERT INTO jobs (company,title) VALUES ('ABC Company','JS Developer');
+
+SELECT * FROM "jobs" LIMIT 1000;
--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const {NODE_ENV} = require('./config');
+const knex = require('knex');
+const {NODE_ENV, DATABASE_URL} = require('./config');
 
 const app = express();
 
@@ -12,6 +13,14 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+
+const db = knex({
+    client: 'pg',
+    connection: DATABASE_URL
+});
+
+app.set('db',db);
+
 
 app.get('/', (req,res)=>{
     res.send('JobTracker');
